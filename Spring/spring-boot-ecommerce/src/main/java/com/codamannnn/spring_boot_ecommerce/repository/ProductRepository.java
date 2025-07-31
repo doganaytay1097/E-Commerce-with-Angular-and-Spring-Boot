6d3fec1c7238c2e8719d35a5232ef27d0ff501e9
Doğanay Tay <116439695+doganaytay1097@users.noreply.github.com>
2025-07-31 11:25:44 +0300

    Delete Spring/spring-boot-ecommerce/src/main directory

--- Spring/spring-boot-ecommerce/src/main/java/com/codamannnn/spring_boot_ecommerce/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codamannnn.spring_boot_ecommerce.repository;
-
-import com.codamannnn.spring_boot_ecommerce.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ProductRepository extends JpaRepository<Product,Long> {
-}
